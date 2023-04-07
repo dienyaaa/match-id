15fb73e52be0e771ae18234bb4c5fc305dcdc809
dienyaaa <40639641+dienyaaa@users.noreply.github.com>
2023-04-07 12:14:18 +0500

    java spring boot 4 webpages need to refuse from pyscript in main page

---
+++ src/main/java/com/match/calculator/controllers/MainController.java
@@ -0,0 +1,27 @@
+package com.match.calculator.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Рассчитайте Ваш размер");
+        return "home";
+    }
+
+    @GetMapping("/tables")
+    public String tables(Model model) {
+        model.addAttribute("title", "Таблица размеров одежды");
+        return "tables";
+    }
+
+    @GetMapping("/measure")
+    public String measure(Model model) {
+        model.addAttribute("title", "Как определить свой размер одежды");
+        return "measure";
+    }
+}
